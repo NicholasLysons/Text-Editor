49cc8bcd2a940d8213247b565240cf050a5871a3
NicholasLysons <boylysons@icloud.com>
2023-05-11 13:46:57 -0700

    fixed webpack and package lock.json

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,7 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -18,13 +17,39 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
-
+      //webpack plugin for html files
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Text Editor",
+      }),
+      //for custom service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      //creates manifest.json 
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Text Editor",
+        short_name: "Editor",
+        description: "Edit text for notes or other daily needs",
+        background_color: "",
+        theme_color: "",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons")
+          } 
+        ],
+      }),
+    ], 
     module: {
-      rules: [
-        
-      ],
+      rules: [    
+    ],
     },
   };
 };
