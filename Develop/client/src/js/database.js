6c8e9f6c9ec29cdda0e8f7f36b4bd691996dc627
NicholasLysons <boylysons@icloud.com>
2023-05-11 18:32:16 -0700

    finished databases

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,16 +12,35 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async () => console.log(); 
+//allows put in database
+export const putDb = async (text) => {
 
+  const textDb = await openDB('texteditor-db', 1);
 
-//console.error('putDb not implemented');
+  const tx = textDb.transaction('texteditor-db', 'readwrite');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.log('Get from database');
+  const store = tx.objectStore('texteditor-db');
 
+  const request = store.add({id:1, value: text});
 
-//console.error('getDb not implemented');
+  const result = await request;
+  console.log('data saved', result);
+};
+
+//allows get in database
+export const getDb = async () => {
+  
+  const textdb = await openDB('texteditor-db', 1);
+
+  const tx = textdb.transaction('texeditor-db', ' readonly');
+
+  const store = tx.objectStore('texteditor-db');
+
+  const request = store.get(1);
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
